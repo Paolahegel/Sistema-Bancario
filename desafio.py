da7988b2f22d1394b9af7b8b121367beb87ac6bd
Paola Moura <169110243+Paolahegel@users.noreply.github.com>
2024-12-16 19:23:07 -0300

    Update desafio.py

--- desafio.py
+++ desafio.py
@@ -29,14 +29,40 @@ while True:
             print("Operação falhou! Informe um valor válido!")
 
     elif opcao == 2:
-         valor_saque = float
-         print("Limite")
+         valor_saque = float(input("Informe o valor do saque: "))
 
-    # elif opcao == 3:
-    #     print("Extrato")
+         excedeu_saldo = valor_saque > saldo
 
-    # elif opcao == 0:
-    #     break
+         excedeu_limite = valor_saque > limite
 
-    # else:
-    #     print("Operação inválida, selecione a operação desejada novamente.")
+         excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+         if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+         elif excedeu_limite:
+            print("Operação falhou! O valor do saque execede o limite.")
+
+         elif excedeu_saques:
+            print("Operação falhou!Número máximo de saques excedido!")
+         
+         elif valor_saque > 0:
+            saldo -= valor_saque
+            extrato += f"Saque: R$ {valor_saque: .2f}\n"
+            numero_saques += 1
+            print("Saque realizado com sucesso")
+            
+    else:
+        print("Operação falhou! O valor informado é inválido!")
+
+    if opcao == 3:        
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo atual: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == 0:
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
